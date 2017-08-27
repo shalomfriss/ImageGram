7474a5beec631e49a9d509e1c80292e0cd6c36ed
Shalom Friss <shalom.friss@climate.com>
2017-08-27 15:50:07 -0500

    Added readme instruction

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ImageGram
 Simple HTMLElement extensions with instagram style filters. These can be used interchangeably with an img tag. 
+For examples look at index.html
--- classes/ImageBase.js
+++ classes/ImageBase.js
@@ -6,18 +6,9 @@ class ImageBase extends HTMLElement {
        super();
    }
    
-   collect(){
-    	var div = this.children[1];
-		var img = div.children[0];
-	
-		for(var i = 0; i < this.attributes.length; i++)
-		{
-			var att = this.attributes[i];
-			img.setAttribute(att.name, att.value);
-		}
-   }
    
-   collect0(){
+   
+   collect(){
     	var div = this.children[1];
 		var img = this.children[0];
 	
@@ -28,22 +19,14 @@ class ImageBase extends HTMLElement {
 		}
    }
    
-   collect1(){
-		var img = this.children[1];
-	
-		for(var i = 0; i < this.attributes.length; i++)
-		{
-			var att = this.attributes[i];
-			img.setAttribute(att.name, att.value);
-		}
-   }
+   
    
    createdCallback(){
 	   this.innerHTML = ""+
 			"<img/><div></div>"+
         	"";
 		 
-		this.collect0();
+		this.collect();
    }
    
 }
